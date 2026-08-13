421cd3a7d31be771548964019cfcc7620b6d2226
BalkesVedat <80820828+BalkesVedat@users.noreply.github.com>
2026-08-13 21:44:06 +0300

    13082026_2144

--- Konsol/KonsolUygulamalarim/Döngüler/Program.cs
+++ Konsol/KonsolUygulamalarim/Döngüler/Program.cs
@@ -6,6 +6,24 @@ using System.Threading.Tasks;
 
 namespace Donguler
 {
+    public enum Kategori
+    { 
+        Gıda,
+        Elektronik,
+        Diğer
+    }
+
+    public class Urun
+    {
+        public string UrunAd { get; set; }
+        public double Fiyat { get; set; }
+        public int Adet { get; set; }
+        public Kategori kategori { get; set; }
+        public double KDV { get; set; }
+        public double KDVDahilTutar { get; set; }
+    }
+
+
     internal class Program
     {
         static void Main(string[] args)
@@ -14,15 +32,15 @@ namespace Donguler
             //For döngüsü: Bir kod için çalıştırılacak adet belli ise tercih ettiğimiz yapıdır. Çünki en hızlı çalışan yapıdır.
 
             Console.WriteLine("1-10 arası sayılar ---------");
-            
-            for (int i=1;i<=10;i++)
+
+            for (int i = 1; i <= 10; i++)
             {
                 Console.WriteLine(i);
             }
 
             Console.WriteLine(" iller listesi ---------------");
 
-            string[] iller = {"Kastamonu", "Manisa", "Adıyaman", "Konya", "Mersin", "Siirt" };
+            string[] iller = { "Kastamonu", "Manisa", "Adıyaman", "Konya", "Mersin", "Siirt" };
 
             for (int i = 0; i < iller.Length; i++)
             {
@@ -34,15 +52,15 @@ namespace Donguler
             int toplam = 0;
 
             //1. yöntem
-            for (int i = 0; i <= 100; i+=2)
+            for (int i = 0; i <= 100; i += 2)
             {
                 toplam += i;
             }
 
             //2.yöntem
-            for (int i = 0; i <= 100; i++) 
+            for (int i = 0; i <= 100; i++)
             {
-                if (i%2==0)
+                if (i % 2 == 0)
                 {
                     toplam += i;
                 }
@@ -50,7 +68,7 @@ namespace Donguler
 
             Console.WriteLine(toplam);
 
-            Dictionary<string,double> urunler = new Dictionary<string,double>();
+            Dictionary<string, double> urunler = new Dictionary<string, double>();
 
             urunler.Add("domates", 150000);
             urunler.Add("havuç", 37000);
@@ -59,7 +77,7 @@ namespace Donguler
 
             double toplamTutar = 0;
 
-           // List<double> fiyatlar = urunler.Values.ToList();
+            // List<double> fiyatlar = urunler.Values.ToList();
 
             for (int i = 0; i < urunler.Count; i++)
             {
@@ -107,6 +125,178 @@ namespace Donguler
                 Console.WriteLine(item);
             }
 
+            Console.WriteLine("-- WHILE Döngüsü ------------- ");
+
+            // While: Bir koşula bağlı olarak bir işlemin tekrar tekrar (koşul doğru olduğu müddetçe) yapılması gerekiyor ise, bu işlem için en uygun döngü modelidir. 
+
+            //
+            //while (koşul)
+            //{
+            // Koşul doğru olduğu müddetçe çalışacak işlem
+            //}
+
+            //while (true)  // Sonsuz döngü
+            //{
+
+            //}
+
+            // Kullanıcıdan alınan 2 sayıyı toplayarak sonucunu gösteren ve kullanıcı devam etmek istediği müddetçe tekrar tekrar çalışan program
+
+            double sayi1 = 0, sayi2 = 0, sonuc = 0;
+            char cevap = 'E';
+
+            while (cevap == 'E')
+            {
+                Console.Write("1. sayıyı giriniz :");
+                sayi1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("2. sayıyı giriniz :");
+                sayi2 = Convert.ToDouble(Console.ReadLine());
+
+                sonuc = sayi1 + sayi2;
+
+                Console.WriteLine($"{sayi1}+{sayi2}={sonuc}");
+
+                Console.Write("Devam Etmek İstiyor musunuz? (E/H) : ");
+
+                cevap = (char)(Console.ReadLine().ToUpper()[0]);
+
+                //char[] gelenler = Console.ReadLine().ToCharArray();
+
+                //cevap = gelenler[0];
+            }
+
+            //while (true)
+            //{
+            //    Console.WriteLine("Merhaba!");
+            //}
+
+            int sayac = 1;
+            double toplam2 = 0;
+
+            while (sayac <= 100)
+            {
+                toplam2 += sayac;
+                sayac++;
+            }
+
+            Console.WriteLine($"Toplam : {toplam2}");
+
+            Console.WriteLine("-- DO - WHILE Döngüsü  ------------- ");
+            //do-while : ilk çalışmada koşul kontrolü yapmadan işlemi çalıştırır ve sonra tekrar çalışıp çalışmayavağını belirlemek için koşul kontrolü yapar. 
+
+            //do
+            //{
+            // işlemler
+            //} while (koşul);
+
+            do
+            {
+                Console.Write("1. sayıyı giriniz :");
+                sayi1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("2. sayıyı giriniz :");
+                sayi2 = Convert.ToDouble(Console.ReadLine());
+
+                sonuc = sayi1 + sayi2;
+
+                Console.WriteLine($"{sayi1}+{sayi2}={sonuc}");
+
+                Console.Write("Devam Etmek İstiyor musunuz? (E/H) : ");
+
+                cevap = (char)(Console.ReadLine().ToUpper()[0]);
+
+                //char[] gelenler = Console.ReadLine().ToCharArray();
+
+                //cevap = gelenler[0];
+            } while (cevap == 'E');
+
+
+            toplam2 = 0;
+            sayac = 0;
+
+            do
+            {
+                toplam2 += sayac;
+                sayac++;
+            }while (sayac <= 100) ;
+
+            Console.WriteLine($"Toplam : {toplam2}");
+
+            // ürün adı girin
+            // ürün fiyatı gir
+            // ürün miktarı gir 
+            // kategorisini gir
+            // eğer kategori
+            // "gıda" ise kdv oranı 0.01 
+            // "elektronik" ise kdv 0.2
+            // "diğer" ise kdv 0.1 hesaplanacak
+            // "yeni giriş?" sorulsun
+            // "evet" ise tekrar başa dön
+            // "hayır" ise girilmiş olan ürün adedi ve tüm ürünlerin kdv dahil toplam değeri ekrana yazılsın.
+
+
+            //-------------------------------------
+
+            Console.WriteLine(" Örnek -------------- : ");
+
+            string devam = "";
+
+            List<Urun> urunListesi = new List<Urun>();
+
+            do
+            {
+                Urun u = new Urun();
+
+                Console.Write("Urun Adı :");
+                u.UrunAd = Console.ReadLine();
+                Console.Write("Urun Adedi :");
+                u.Adet = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Urun Fiyat :");
+                u.Fiyat = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Kategori (Gıda-G/Elektronik-E, Diğer-D) ? :");
+                string gelen = Console.ReadLine().ToUpper();
+
+                switch (gelen)
+                {
+                    case "G":
+                        u.kategori = Kategori.Gıda;
+                        u.KDV = 0.01;
+                        u.KDVDahilTutar = u.Adet * u.Fiyat * (1 + u.KDV);
+                        break;
+                    case "E":
+                        u.kategori = Kategori.Elektronik;
+                        u.KDV = 0.2;
+                        u.KDVDahilTutar = u.Adet * u.Fiyat * (1 + u.KDV);
+                        break;
+                    default:
+                        u.KDV = 0.1;
+                        u.KDVDahilTutar = u.Adet * u.Fiyat * (1 + u.KDV);
+                        u.kategori = Kategori.Diğer;
+                        break;
+                }
+
+                urunListesi.Add(u);
+
+                Console.Write("Devam edilsin mi? (E/H) ");
+                devam = Console.ReadLine().ToUpper();
+
+            } while (devam=="E");
+
+
+            double toplamUrunTutar = 0;
+            double toplamAdet = 0;
+
+            foreach (var item in urunListesi)
+            {
+                toplamUrunTutar += item.KDVDahilTutar;
+                toplamAdet += item.Adet;
+            }
+
+            Console.WriteLine($"Satır:{urunListesi.Count} + Urun Adedi:{toplamAdet} - Toplam Tutar:{toplamUrunTutar} ");
+
+            Console.ReadKey();
 
 
         }

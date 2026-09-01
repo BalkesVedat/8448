0e2dd38c85f17664999205e9a9528c8d4256a54f
BalkesVedat <80820828+BalkesVedat@users.noreply.github.com>
2026-09-01 21:35:10 +0300

    01092026_2135

---
+++ Desktop/WindowsDesktop/HelloWorld/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWorld
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+
+        public void MesajVer(object sender, EventArgs e)
+        {
+            MessageBox.Show("Merhaba", "Hoşgeldiniz", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+    }
+}
